bb348f260e8a6072fbda9503e6caa071836f9008
Simon Frankau <sgf@arbitrary.name>
2018-05-01 00:02:02 +0100

    Add in missing "break"s.

--- src/galasm.c
+++ src/galasm.c
@@ -2139,12 +2139,16 @@ WriteFuseFile(char *filename, int gal_type)
 	{
 	case GAL16V8:
 		num_of_col = MAX_FUSE_ADR16 + 1;
+		break;
 	case GAL20V8:
 		num_of_col = MAX_FUSE_ADR20 + 1;
+		break;
 	case GAL20RA10:
 		num_of_col = MAX_FUSE_ADR20RA10 + 1;
+		break;
 	case GAL22V10:
 		num_of_col = MAX_FUSE_ADR22V10 + 1;
+		break;
 	}
 
 	if ((fp = fopen(filename, (char *) "w")))
